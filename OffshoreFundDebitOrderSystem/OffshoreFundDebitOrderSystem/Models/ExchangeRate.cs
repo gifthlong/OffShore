6922bf7d2a4759e36689677282bd1d401dc957f7
Gift Hlongoane <jabu.hlong@gmail.com>
2015-10-07 11:16:23 +0200

    Added ExchangeRate.cs

---
+++ OffshoreFundDebitOrderSystem/OffshoreFundDebitOrderSystem/Models/ExchangeRate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OffshoreFundDebitOrderSystem.Models
+{
+    public class ExchangeRate
+    {
+        public string name { get; set; }
+        public decimal rate { get; set; }
+    }
+}
